675f8688fddb610cfc9574883533baaed2251902
jwalla7 <j7wallace7@outlook.com>
2022-03-17 15:44:04 -0500

    Mar 17 0343

--- src/Loops/For.java
+++ src/Loops/For.java
@@ -31,22 +31,20 @@ public class For {
                          Type in SUM to run Two Sum!
                          """
                           );
-//          2.2 - (Define) accepted user inputs
         String fizzBuzzResponse = "FIZZ";
         String twoSumResponse = "SUM";
-//          2.3 - (Implementation) of Scanner to receive user input.
         Scanner console = new Scanner(System.in);
         String response;
         int target;
-        int[] capacity;
+        int capacity;
         int capacityInt;
+        int [] randomNumbersArray;
 
         do {
             response = console.nextLine();
-//          2.4 - (What if?) user input matches accepted user inputs 2.2
+
             if (response.equals(fizzBuzzResponse) || response.equals(twoSumResponse)) {
                 break;
-//          2.5 - (What if?) user inputs doesn't match accepted user inputs 2.2: run error message
             } else {
                 System.out.println(
                                  """
@@ -56,69 +54,107 @@ public class For {
                                  """
                                   );
             }
-//         2.6 - Keep repeating 2.4 error message if 2.4 persists.
         } while (true);
 
         if (response.equals(fizzBuzzResponse)) {
-//         2.7 - (What if?) user inputs match accepted user inputs 2.2
             System.out.println(
-                    """
-                            Lets run FizzBuzz!
-                            FizzBuzz will generate a list of numbers up to the number you input.
-                            FizzBuzz will then:
-                            1. replace all numbers in the list divisible by 3 with the phrase "Fizz"
-                            2. replace all numbers in the list divisible by 5 with the phrase "Buzz"
-                            3. replace all numbers in the list divisible by both 3 and 5 with the phrase "FizzBuzz"
-                            Let's start!
-                            Enter a number greater than 15
-                            """
-            );
-        }
-        do {
-            capacityInt = console.nextInt();
-            try {
-                if (capacityInt > 15) {
-                    break;
-                } else {
-                    System.out.println(
-                            "Invalid, enter a number larger than " + capacityInt + "."
-                    );
+                             """
+                             Lets run FizzBuzz!
+                             FizzBuzz will generate a list of numbers up to the number you input.
+                             FizzBuzz will then:
+                             1. replace all numbers in the list divisible by 3 with the phrase "Fizz"
+                             2. replace all numbers in the list divisible by 5 with the phrase "Buzz"
+                             3. replace all numbers in the list divisible by both 3 and 5 with the phrase "FizzBuzz"
+                             Let's start!
+                             Enter a number greater than 15
+                             """
+                              );
+            do {
+                capacityInt = console.nextInt();
+                try {
+                    if (capacityInt > 15) {
+                        break;
+                    } else {
+                        System.out.println( "Invalid, enter a number larger than " + capacityInt + "." );
+                    }
+                } catch (InputMismatchException eInput) {
+                    System.out.println( "Invalid, enter a number larger than 15." );
+                } catch (Exception e) {
+                    System.out.println( "Invalid, enter a number larger than " + capacityInt + "." );
+                } finally {
+                    response = console.nextLine();
                 }
-            } catch (InputMismatchException eInput) {
-                System.out.println(
-                        "Invalid, enter a number larger than 15."
-                );
-            } catch (Exception e) {
-                System.out.println(
-                        "Invalid, enter a number larger than " + capacityInt + "."
-                );
-            } finally {
-                response = console.nextLine();
-            }
-        } while (true);
-        System.out.println(
-                "Calculating numbers up to " + capacityInt + " ..." + '\n'
-                + "Calculating FIZZ... " + '\n'
-                + "Calculating BUZZ... " + '\n'
-                + "Calculation FIZZBUZZ..."
-        );
-        System.out.println(fizzBuzzMethod(capacityInt));
+            } while (true);
+            System.out.println(
+                             "Calculating numbers up to " + capacityInt + " ..." + '\n'
+                            + "Calculating FIZZ... " + '\n'
+                            + "Calculating BUZZ... " + '\n'
+                            + "Calculation FIZZBUZZ..."
+                             );
+            System.out.println(fizzBuzzMethod(capacityInt));
+        }
 
         if (response.equals(twoSumResponse)) {
             System.out.println(
                              """
-                            Lets run Two Sum!
-                            Two Sum will generate an array of random numbers and will accept a target number.
-                            Two sum will then find 
-                            Two Sum will then:
-                            1. replace all numbers in the list divisible by 3 with the phrase "Fizz"
-                            2. replace all numbers in the list divisible by 5 with the phrase "Buzz"
-                            3. replace all numbers in the list divisible by both 3 and 5 with the phrase "FizzBuzz"
-                            Let's start!
-                            Enter a number greater than 15
-                            """
-            );
+                             Lets run Two Sum!
+                             1. Two Sum will generate an array of random integers and will accept a target integer.
+                             2. Two sum will then find the first occurrence of two random integers equal to the target integer.
+                             3. Two sum will return the indices of the first occurrence.
+                             Let's start!
+                             Enter a number greater than 3
+                             """
+                              );
         }
+
+        do {
+            capacity = console.nextInt();
+            try {
+                if (capacity > 3) {
+                    break;
+                } else {
+                    System.out.println( "Invalid, enter a integer larger than " + capacity + "." );
+                }
+            } catch (InputMismatchException eMis) {
+                System.out.println( "Invalid, enter a integer larger than 3." );
+            } catch (Exception e) {
+                System.out.println( "Invalid, enter a integer larger than 3." );
+            } finally {
+                response = console.nextLine();
+            }
+        } while (true);
+
+        randomNumbersArray = generateRandomNumbers(capacity);
+        System.out.println(
+                         "Calculating " + capacity + " random integers..." + '\n'
+                         + "Creating integers..." + Arrays.toString(randomNumbersArray) + '\n'
+                         + """
+                           Now, lets enter in an integer you want the sum of two random integers to equal.
+                           Enter in an integer greater than 0.
+                         """
+                          );
+        do {
+            target = console.nextInt();
+            try {
+                if (target > 0) {
+                    break;
+                } else {
+                    System.out.println( "Invalid, enter a integer larger than " + target + "." );
+                }
+            } catch (InputMismatchException eMis) {
+                System.out.println( "Invalid, enter a integer larger than 0." );
+            } catch (Exception e) {
+                System.out.println( "Invalid, enter a integer larger than 0." );
+            } finally {
+                response = console.nextLine();
+            }
+        } while (true);
+
+        System.out.println(
+                         "Searching through list of random integers... " + Arrays.toString(randomNumbersArray) + '\n'
+                                 + "Finding first occurrence when the sum of two random integers equals " + target + " ..."
+                          );
+        System.out.println(Arrays.toString(twoSumMethod(randomNumbersArray, target)));
     }
 
     //    FizzBuzz Method
@@ -128,13 +164,10 @@ public class For {
         for (int number = 1; number <= capacity; number++) {
             if (number % 15 == 0) {
                 fizzBuzzData.add("FizzBuzz");
-
             } else if (number % 5 == 0) {
                 fizzBuzzData.add("Buzz");
-
             } else if (number % 3 == 0) {
                 fizzBuzzData.add("Fizz");
-
             } else {
                 fizzBuzzData.add(String.valueOf(number));
             }
@@ -142,8 +175,34 @@ public class For {
         return fizzBuzzData;
     }
 
+//    Random Integers Array Generator Method
+    public static int[] generateRandomNumbers (int capacity) {
+        int [] generatedArray = new int[capacity];
+        Random randomNumberGenerator = new Random();
+
+        for (int integers = 0; integers < generatedArray.length; integers++) {
+            generatedArray[integers] = randomNumberGenerator.nextInt(capacity);
+        }
+        return generatedArray;
+    }
+
     //      Two Sum Method
-    public static int[] twoSumMethod (int[] capacity, int target) {
-        return null;
+    public static int[] twoSumMethod (int [] capacity, int target) {
+        Map<Integer, Integer> consolidate = new HashMap<>();
+        int [] occurrence = new int [2];
+
+        for (int integer = 0; integer < capacity.length; integer++) {
+            if (consolidate.containsKey(target - capacity[integer])) {
+                occurrence[1] = integer;
+                occurrence[0] = consolidate.get(target - capacity[integer]);
+
+                return occurrence;
+            }
+            consolidate.put(capacity[integer], integer);
+        }
+        if (occurrence[0] == 0 && occurrence[1] == 0) {
+            System.out.println(" There are no instances of Sum Two in this array, try again.");
+        }
+        return occurrence;
     }
 }
