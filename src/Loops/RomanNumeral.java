3fc9728040e1d824f5e46ba69490c407cfef2309
jwalla7 <j7wallace7@outlook.com>
2022-07-01 20:35:08 -0400

    jul 1 0835

--- src/Loops/RomanNumeral.java
+++ src/Loops/RomanNumeral.java
@@ -1,43 +1,37 @@
 package Loops;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /*
 *
  * Roman Numeral Conversion
- *
- *
+ *  Create a LinkedHashMap to store Roman Numeral key:value pairs
+ *  Utilize the ForIn Loop to iterate through LinkedHashMap
+ *  Accept a String of Roman Numerals
 *
  */
 public class RomanNumeral {
     public static void main(String[] args) {
-        Map<String, Integer> romanMap = new HashMap();
-        romanMap.put("I", 1);
-        romanMap.put("V", 5);
-        romanMap.put("X", 10);
-        romanMap.put("L", 50);
-        romanMap.put("C", 100);
-        romanMap.put("D", 500);
-        romanMap.put("M", 1000);
+    Map<Character, Integer> roman_numeralsMap = new LinkedHashMap<>();
+        roman_numeralsMap.put('I', 1);
+        roman_numeralsMap.put('V', 5);
+        roman_numeralsMap.put('X', 10);
+        roman_numeralsMap.put('L', 50);
+        roman_numeralsMap.put('C', 100);
+        roman_numeralsMap.put('D', 500);
+        roman_numeralsMap.put('M', 1000);
 
-//        romanMap.forEach(rm -> System.out.println(rm));
-
-        RomanNumeral test_0 = new RomanNumeral();
-        System.out.println(test_0.RomanNumeralConversion("IVXLC"));
+        System.out.println(roman_numeralsMap);
     }
-
 //    Instance variables
-    String romanNumeral = "";
+
 
 //    Constructor
      public RomanNumeral() {}
 //     Methods
-     public String RomanNumeralConversion (String rm_s) {
-         String final_rm = "";
-         for (int r = rm_s.length() - 1; r > rm_s.length(); r--) {
-             System.out.println(rm_s.charAt(r));
-         }
-         return final_rm;
+     public void RomanNumeralConversion (String rm_s) {
      }
 }
