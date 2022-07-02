72ef0fa9dc275bcde27b02bdac56bf2c977f100f
jwalla7 <j7wallace7@outlook.com>
2022-07-02 16:13:04 -0500

    jul 2 0413

--- src/Loops/RomanNumeral.java
+++ src/Loops/RomanNumeral.java
@@ -1,37 +1,55 @@
 package Loops;
 
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 /*
-*
- * Roman Numeral Conversion
- *  Create a LinkedHashMap to store Roman Numeral key:value pairs
- *  Utilize the ForIn Loop to iterate through LinkedHashMap
- *  Accept a String of Roman Numerals
-*
- */
+*   Roman Numeral Conversion
+ *   1.0 Create a LinkedHashMap to store Roman Numeral key:value pairs
+ *   1.1 Utilize the ForIn Loop to iterate through LinkedHashMap
+*    1.2 Accept a String of Roman Numerals*
+*/
+
 public class RomanNumeral {
     public static void main(String[] args) {
-    Map<Character, Integer> roman_numeralsMap = new LinkedHashMap<>();
-        roman_numeralsMap.put('I', 1);
-        roman_numeralsMap.put('V', 5);
-        roman_numeralsMap.put('X', 10);
-        roman_numeralsMap.put('L', 50);
-        roman_numeralsMap.put('C', 100);
-        roman_numeralsMap.put('D', 500);
-        roman_numeralsMap.put('M', 1000);
-
-        System.out.println(roman_numeralsMap);
+        System.out.println(roman_numeralMap);
+        RomanNumeral test_0 = new RomanNumeral();
+        test_0.RomanNumeralConversion("LVII");
     }
-//    Instance variables
 
+    static Map<Character, Integer> roman_numeralMap = new LinkedHashMap<>() {{
+    /*
+    *    Anonymous Inner Class (Static Initializer/Block) - Static Initializers are run before main method, constructors and methods.
+     *     The first brace creates a new Anonymous Inner Class.
+     *     The second declares an "Instance Initializer" block that will run when
+    *      the Anonymous Inner Class is instantiated.
+     */
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
 
 //    Constructor
      public RomanNumeral() {}
+
 //     Methods
-     public void RomanNumeralConversion (String rm_s) {
+     public void RomanNumeralConversion (String rm_string) {
+         char[] rm_chars = new StringBuilder(rm_string) //  Reverse String and convert to a char[] array
+                 .reverse()
+                 .toString()
+                 .toCharArray();
+
+         for (char rm_char : rm_chars) {
+//             System.out.println(rm_char);
+             if (roman_numeralMap.containsKey(rm_char)) {
+                 System.out.println(roman_numeralMap.get(rm_char));
+             }
+         }
      }
+
 }
